ea28c744388fa29d80cc3f3997f2383bf9fda35d
tulf <timzulf@gmail.com>
2018-09-21 11:46:38 -0400

    alrightly we good can get jokes with actual valid toke, copypaste is hard lol. Also you apparently have to specify the request as a json object in the header, who knew?

--- config/middlewares.js
+++ config/middlewares.js
@@ -5,6 +5,7 @@ const jwtKey = require('../_secrets/keys').jwtKey;
 // quickly see what this file exports
 module.exports = {
   authenticate,
+  newToken
 };
 
 // implementation details
@@ -24,4 +25,18 @@ function authenticate(req, res, next) {
       error: 'No token provided, must be set on the Authorization Header',
     });
   }
-}
+};
+
+//create token,
+function newToken(user) {
+  const payload = {
+    username: user.username
+  };
+  const options = {
+    expiresIn: '1h',
+    jwtid: '45678'
+  };
+  //note that secret is already declared in ./_secrets/keys.jwtkey which is being exported in that file so we only need to sign this.
+  //on a side note given the structure of the token it should go payload,options,secret but I'm not picky it's fine.
+  return jwt.sign(payload, jwtKey, options);
+};
--- config/routes.js
+++ config/routes.js
@@ -1,6 +1,7 @@
 const axios = require('axios');
-
-const { authenticate } = require('./middlewares');
+const bcrypt = require('bcryptjs');
+const db = require('../database/dbConfig')
+const { authenticate, newToken } = require('./middlewares');
 
 module.exports = server => {
   server.post('/api/register', register);
@@ -8,13 +9,60 @@ module.exports = server => {
   server.get('/api/jokes', authenticate, getJokes);
 };
 
+//So it seems like I need extra middleware to generate a token, why the instructions would leave that out but build all of this extra shit is beyond me, but okay there is 20 minutes I'm not getting back.
+//Also it's annoying to me
 function register(req, res) {
   // implement user registration
-}
+  const credentials = req.body;
+       const hashnum = bcrypt.hashSync(credentials.password, 14);
+       credentials.password = hashnum;
+
+       db("users")
+         .insert(credentials)
+         .then(users => {
+           const id = users[0];
+           db('users')
+             .where({id})
+             .first()
+             .then(user => {
+               const token = newToken(user);
+               res.status(201).json({id: user.id, token});
+
+             })
+
+             .catch(err => res.status(500).send(err));
+         });
+     }
+
 
 function login(req, res) {
   // implement user login
-}
+  const credentials = req.body;
+        db("users")
+          .where({
+            username: credentials.username
+          })
+          .first()
+          .then(user => {
+            if (user && bcrypt.compareSync(credentials.password, user.password)) {
+              //pulling username off user into the cookie
+              const token = newToken(user)
+
+
+              res.status(200).json({
+                token
+              });
+              //send dat token back
+              //not sure why I can't get
+            } else {
+              res.status(401).json({
+                message: "They wanna see me go AY, all you gotta do is speak on ye!"
+              });
+            }
+          })
+          .catch(err => res.status(500).send(err));
+      }
+
 
 function getJokes(req, res) {
   axios
